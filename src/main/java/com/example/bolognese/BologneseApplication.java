95d10e4b41709646749ea0d710089d4db9a83e5e
rubashka <fullbanana@naver.com>
2024-11-13 20:06:56 +0900

    Initial commit

---
+++ src/main/java/com/example/bolognese/BologneseApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolognese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BologneseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BologneseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bolognese/BologneseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bolognese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BologneseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
